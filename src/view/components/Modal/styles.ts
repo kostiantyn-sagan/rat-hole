da3b742b1c4a60dcccbf31d3faf45dd3b7825d47
kostiantyn-sagan <kstelephone8@gmail.com>
2021-11-17 18:55:23 +0300

    Добавляет адаптивность

--- src/view/components/Modal/styles.ts
+++ src/view/components/Modal/styles.ts
@@ -14,11 +14,19 @@ export const Content = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  max-width: 600px;
   width: 100%;
+  max-width: 280px;
   padding: 12px;
   background-color: #fff;
   border-radius: 3px;
   box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
     0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+
+  @media screen and (min-width: 480px) {
+    max-width: 450px;
+  }
+
+  @media screen and (min-width: 1200px) {
+    max-width: 600px;
+  }
 `;
